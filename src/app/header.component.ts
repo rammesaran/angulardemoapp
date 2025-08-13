2cd8193f136f2957ac33d99950593c0220de291c
ramkumar <ramkumar@palebluesoftware.com>
2025-08-13 21:29:43 +0530

    initalcommit

---
+++ src/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'myapp-header',
+    templateUrl: './header.component.html',
+    standalone: true,
+    styleUrl:'./header.component.css',
+
+})
+
+export class HeaderComponent{
+
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './app/header.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+
+  let name = "ram";
+ console.log(name.endsWith('c'));
+
+
+
