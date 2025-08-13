977d225b9a9136534586af40c6a5b5339e856257
ramkumar <ramkumar@palebluesoftware.com>
2025-08-14 01:09:11 +0530

    signals for input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,computed,Input,input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomindex = Math.floor(Math.random()* DUMMY_USERS.length);
@@ -13,12 +13,20 @@ const randomindex = Math.floor(Math.random()* DUMMY_USERS.length);
 export class UserComponent {
   
 
-@Input({required: true}) avatar!: string;
-@Input({ required: true}) name!: string;
+// @Input({required: true}) avatar!: string;
+// @Input({ required: true}) name!: string;
+avatar = input.required<string>();
+name = input.required<string>();
 
-get imagePath(){
-  return 'assets/users/' +this.avatar;
-}
+// imagePath = computed(()=> {
+//   return 'assets/users/' +this.avatar()
+// });
+
+imagePath = computed(() => 'assets/users/' +this.avatar());
+
+// get imagePath(){
+//   return 'assets/users/' +this.avatar;
+// }
 onSelectUser(){
 
 }
